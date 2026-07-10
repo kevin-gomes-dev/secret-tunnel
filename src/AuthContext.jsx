ce42516109b63f5e154765990064d9b719e7cda2
Kevin Gomes <kevin.gomes.dev@gmail.com>
2026-07-09 22:13:33 -0400

    base solution

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,18 +1,46 @@
 import { createContext, useContext, useState } from "react";
 
 const API = "https://fsa-jwt-practice.herokuapp.com";
-
+// The locations, add to here if addinig another location
+const LOCATIONS = { gate: "GATE", tablet: "TABLET", tunnel: "TUNNEL" };
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [token, setToken] = useState();
-  const [location, setLocation] = useState("GATE");
+  const [location, setLocation] = useState(LOCATIONS.gate);
 
   // TODO: signup
-
+  async function signup(username) {
+    try {
+      const req = await fetch(API + "/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username }),
+      });
+      const result = await req.json();
+      if (req.ok) {
+        setToken(result.token);
+        setLocation(LOCATIONS.tablet);
+      }
+    } catch (e) {
+      console.log("Error:", e);
+    }
+  }
   // TODO: authenticate
+  async function authenticate() {
+    try {
+      if (!token) throw Error("No token");
+      const req = await fetch(API + "/authenticate", {
+        method: "GET",
+        headers: { "Content-Type": "application/json", Authorization: "Bearer " + token },
+      });
+      if (req.ok) setLocation(LOCATIONS.tunnel);
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
-  const value = { location };
+  const value = { location, authenticate, signup };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
--- src/Tablet.jsx
+++ src/Tablet.jsx
@@ -1,22 +1,19 @@
+import { useAuth } from "./AuthContext";
+
 /** Button that attempts to use the token in context when clicked */
 export default function Tablet() {
   // TODO: call authenticate when form is submitted
-
+  const { authenticate } = useAuth();
   return (
     <section>
       <p>
-        The sound of your name thuds against the gate as the two badgers furrow
-        their brows. The badger on the right beckons you to approach.
+        The sound of your name thuds against the gate as the two badgers furrow their brows. The badger on the right
+        beckons you to approach.
       </p>
       <p>"Only those who are pure of heart may pass."</p>
-      <p>
-        "Place your hand upon this stone tablet, and thus will your true self be
-        revealed."
-      </p>
-      <p>
-        It holds out a rectangular stone tablet carved with an intricate design.
-      </p>
-      <form>
+      <p>"Place your hand upon this stone tablet, and thus will your true self be revealed."</p>
+      <p>It holds out a rectangular stone tablet carved with an intricate design.</p>
+      <form action={authenticate}>
         <button>Place your palm upon the tablet.</button>
       </form>
     </section>
